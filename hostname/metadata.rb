0348d384b2b23e9b1f1153fb9ca09fb76d775198
Jacques Marneweck <jacques@powertrip.co.za>
2013-07-26 21:36:04 +0000

    Updated meta
    Signed-off-by: Jacques Marneweck <jacques@powertrip.co.za>

--- hostname/metadata.rb
+++ hostname/metadata.rb
@@ -1,6 +1,13 @@
+name             "hostname"
 maintainer       "Jacques Marneweck"
 maintainer_email "jacques@powertrip.co.za"
 license          "All rights reserved"
 description      "Installs/Configures hostname"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+%w{
+  smartos
+}.each do |os|
+  supports os
+end
