ffaec9043bc1f6ba3fae37ab4ea8c0efa2e80d16
Jacques Marneweck <jacques@powertrip.co.za>
2013-11-11 07:01:20 +0000

    Update datacenter metadata
    Signed-off-by: Jacques Marneweck <jacques@powertrip.co.za>

--- datacenter/metadata.rb
+++ datacenter/metadata.rb
@@ -1,6 +1,13 @@
-maintainer       "Jacques Marneweck"
-maintainer_email "jacques@powertrip.co.za"
-license          "All rights reserved"
-description      "Installs/Configures datacenter"
+name             'datacenter'
+maintainer       'Jacques Marneweck'
+maintainer_email 'jacques@powertrip.co.za'
+license          'All rights reserved'
+description      'Sets some variables to set which datacenter a headnode/compute node resides in'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          '0.0.1'
+
+%w{
+  smartos
+}.each do |os|
+  supports os
+end
