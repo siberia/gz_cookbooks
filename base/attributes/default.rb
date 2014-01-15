0479a3a99217accffb864e27217f223660621e93
Jacques Marneweck <jacques@powertrip.co.za>
2014-01-15 07:45:07 +0000

    Attributes to specify ntp servers
    Signed-off-by: Jacques Marneweck <jacques@powertrip.co.za>

--- base/attributes/default.rb
+++ base/attributes/default.rb
@@ -2,12 +2,19 @@
 # Cookbook Name:: base
 # Attributes:: default
 #
-# Copyright 2013, Jacques Marneweck
+# Copyright 2013-2014, Jacques Marneweck
 #
 # All rights reserved - Do Not Redistribute
 #
 
 default['base'] = {
+  'ntp' => {
+    'servers' => [
+      '0.pool.ntp.org',
+      '1.pool.ntp.org',
+      '2.pool.ntp.org'
+    ]
+  },
   'pkgsrc_version' => '2013Q3',
   'pkgsrc_arch' => 'i386'
 }
